30cc2852ac6900d5c962883621c729776db446d0
Celested34 <celested34@gmail.com>
2022-11-08 15:05:09 -0800

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
-
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
-
+import Header from "./components/Header";
+import withContext from "./Context";
 import Courses from "./components/Courses";
 
 
+
+const HeaderWithContext = withContext(Header);
+
+
+//routes to the components!
 const routes = () => (
   <Router>
     <div>
-    
+    <HeaderWithContext />
       <Switch>
         <Route exact path="/" component={Courses} />
         
@@ -20,3 +24,5 @@ const routes = () => (
 );
 
 export default routes
+
+//Treehouse Set up the React App - starter project files
