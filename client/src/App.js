1fe6473387bfde7a7d8b1b83590b7ed0123992cc
Celested34 <celested34@gmail.com>
2022-11-10 11:39:37 -0800

    comment out other stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,49 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Header from "./components/Header";
+// import Header from "./components/Header";
 
 import Courses from "./components/Courses";
 
 import CourseDetail from "./components/CourseDetail";
-import CreateCourse from "./components/CreateCourse";
-import UpdateCourse from "./components/UpdateCourse";
-import DeleteCourse from "./components/DeleteCourse";
 
-import UserSignUp from "./components/UserSignUp";
-import UserSignIn from "./components/UserSignIn";
-import UserSignOut from "./components/UserSignOut";
+// import CreateCourse from "./components/CreateCourse";
+// import UpdateCourse from "./components/UpdateCourse";
+// import DeleteCourse from "./components/DeleteCourse";
+
+// import UserSignUp from "./components/UserSignUp";
+// import UserSignIn from "./components/UserSignIn";
+// import UserSignOut from "./components/UserSignOut";
 
 import withContext from "./Context";
-import PrivateRoute from "./PrivateRoute";
+// import PrivateRoute from "./PrivateRoute";
 
 //with context
-const UserSignUpWithContext = withContext(UserSignUp); 
-const UserSignInWithContext = withContext(UserSignIn);
+// const UserSignUpWithContext = withContext(UserSignUp); 
+// const UserSignInWithContext = withContext(UserSignIn);
 
-const HeaderWithContext = withContext(Header);
+// const HeaderWithContext = withContext(Header);
 
-const CreateCourseWithContext = withContext(CreateCourse);
-const UpdateCourseWithContext = withContext(UpdateCourse);
-const UserSignOutWithContext = withContext(UserSignOut);
+// const CreateCourseWithContext = withContext(CreateCourse);
+// const UpdateCourseWithContext = withContext(UpdateCourse);
+// const UserSignOutWithContext = withContext(UserSignOut);
 const CourseDetailWithContext = withContext(CourseDetail);
-const DeleteCourseWithContext = withContext(DeleteCourse);
+// const DeleteCourseWithContext = withContext(DeleteCourse);
 
 
 const route = () => (
   <Router>
     <div>
-    <HeaderWithContext />
+    {/* <HeaderWithContext /> */}
       <Switch>
         <Route exact path="/" component={Courses} />
-        <PrivateRoute exact path="/courses/create"  component={CreateCourseWithContext} />
+        {/* <PrivateRoute exact path="/courses/create"  component={CreateCourseWithContext} /> */}
         <Route exact path="/courses/:id" component={CourseDetailWithContext} />
-        <PrivateRoute exact path="/courses/:id/update" component={UpdateCourseWithContext} />
+        {/* <PrivateRoute exact path="/courses/:id/update" component={UpdateCourseWithContext} />
         <Route path="/signup" component={UserSignUpWithContext} />
         <Route path="/signin" component={UserSignInWithContext} />
         <Route path="/signout" component={UserSignOutWithContext} />
-        <PrivateRoute path="/courses/:id/delete" component={DeleteCourseWithContext} />
+        <PrivateRoute path="/courses/:id/delete" component={DeleteCourseWithContext} /> */}
       </Switch>
     </div>
   </Router>
